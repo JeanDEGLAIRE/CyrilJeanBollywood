f92fbd69a999ceace421e9b9396371d99a54d2c2
Jean D <jean.deglaire@gmail.com>
2017-08-09 15:56:49 +0200

    tentative de régler l'affichage des photos

--- src/fr/jean/bollywood/bean/BollywoodController.java
+++ src/fr/jean/bollywood/bean/BollywoodController.java
@@ -1,45 +1,24 @@
 package fr.jean.bollywood.bean;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.model.SelectItem;
 
-import fr.jean.bollywood.model.Actor;
 
 @ManagedBean
 @SessionScoped
 public class BollywoodController {
-
-	private Actor actor;
 	
+	private String actor = "";
 
-
-	public BollywoodController() {
-	}
-
-
-
-	public Actor getActor() {
+	public String getActor() {
 		return actor;
 	}
 
-	public void setActor(Actor actor) {
+	public void setActor(String actor) {
 		this.actor = actor;
 	}
-	
-	public List<SelectItem> getActors() {
-		List<SelectItem> ret = new ArrayList<>();
-		Actor aamir = new Actor("Aamir Khan", "/resources/img/aamir.jpg");
-		Actor karishma = new Actor("Karishma Kapoor", "/resources/img/karishma.jpg");
-		Actor tunak = new Actor("Tunak Tunak Tun !", "/resources/img/tunak.gif");
-		ret.add(new SelectItem(tunak, "Choisir un acteur"));
-		ret.add(new SelectItem(aamir, "Aamir Khan"));
-		ret.add(new SelectItem(karishma, "Karishma Kapoor"));
-		
-		return ret;
-	}
+
+
 
 }
